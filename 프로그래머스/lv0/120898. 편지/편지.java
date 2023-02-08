d4818beeab71e7118afbc2fd7b4e915fbbcbc425
Hye91 <111218964+Hye91@users.noreply.github.com>
2023-02-08 17:20:46 +0900

    [level 0] Title: 편지, Time: 0.05 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        answer = message.length() * 2;
+        //.length는 배열의 길이 , length()는 문자열의 길이 구한다
+        return answer;
+    }
+}
