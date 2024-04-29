f7fbcab1476440e5678b052354602b686922000d
digeuthi <111218964+digeuthi@users.noreply.github.com>
2024-04-29 18:42:12 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc1 = new Scanner(System.in);
+        double A = sc1.nextInt();
+        double B = sc1.nextInt();
+        
+        System.out.println(A/B);
+    }
+}
