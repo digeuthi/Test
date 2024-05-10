8029dac20e88fe1db1661b5fd15dbab5e6eb7393
digeuthi <111218964+digeuthi@users.noreply.github.com>
2024-05-10 16:31:21 +0900

    [Bronze V] Title: 사파리월드, Time: 212 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        long a = sc.nextInt();
+        long b = sc.nextInt();
+        
+        System.out.print(Math.abs(a-b));
+    }
+}
